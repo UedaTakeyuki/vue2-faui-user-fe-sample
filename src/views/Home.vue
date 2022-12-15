4ae80bc360c6d5d0febbd58301abc9a0f06eab5c
Takeyuki UEDA <ueda@latelierdueda.com>
2022-12-16 08:08:03 +0900

    l&f

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
-    Home
+    <span class="text-h4">Home</span>
+    <v-card flat>
+      <v-card-title> Login info</v-card-title>
+      <v-card-text>
+        <p class="ma-0">Login: {{$isLogin}}</p>
+        <p class="ma-0">EmailVerified: {{$isEmailVerified}}</p>
+        <p class="ma-0">DisplayName: {{$displayName}}</p>
+        <p class="ma-0">User Email: {{$user_email}}</p>
+        <p class="ma-0">User ID: {{$user_id}}</p>
+      </v-card-text>
+    </v-card>
   </div>
 </template>
